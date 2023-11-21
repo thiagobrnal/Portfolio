d0e94d5d31c24aafa05965c99d606a6f5999c9cc
Bernal Thiago <thiagoa.bernal@yahoo.com.ar>
2023-11-20 23:41:43 -0300

    agregado de boton modo oscuro/claro

--- src/app.js
+++ src/app.js
@@ -70,3 +70,15 @@ function ocultarDiv(nombreDiv, nombreEnNav){
 }
 
 
+//Boton de modo noctruno/claro
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('mode-btn');
+    console.log(btn);
+
+
+    btn.addEventListener('click', () => {
+        document.body.classList.toggle('dark');
+        console.log('btn clicked');
+      });
+
+});
