e32401f28ca47e0f0045ff4504bfac2d4d048448
Bernal Thiago <thiagoa.bernal@yahoo.com.ar>
2023-11-20 21:19:14 -0300

    agregado de seccion proyectos y contacto

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ function cambiarTexto() {
     }
 }
 
-// Cambia el texto cada 7000 milisegundos (7 segundos)
+// Cambia el texto cada x milisegundos 
 setInterval(animacionTexto, 9100);
 setInterval(cambiarTexto, 10000);
 
@@ -27,9 +27,13 @@ function ocultarDiv(nombreDiv, nombreEnNav){
     let inicio = document.getElementById("inicio")
     let aboutMe = document.getElementById("about-me")
     let habildades = document.getElementById("habilidades")
+    let proyectos = document.getElementById("proyectos")
+    let contacto = document.getElementById("contacto")
     let inicioId = document.getElementById("in")
     let aboutMeId = document.getElementById("about")
     let habilidadesId = document.getElementById("habi")
+    let proyectosId = document.getElementById("proyec")
+    let contactoId = document.getElementById("contac")
     
 
     if(inicio.style.display !== 'none'){
@@ -45,6 +49,16 @@ function ocultarDiv(nombreDiv, nombreEnNav){
     if(habildades.style.display !== 'none'){
         habildades.style.display = 'none'
         habilidadesId.classList.remove('select')
+    }
+
+    if(proyectos.style.display !== 'none'){
+        proyectos.style.display = 'none'
+        proyectosId.classList.remove('select')
+    }
+
+    if(contacto.style.display !== 'none'){
+        contacto.style.display = 'none'
+        contactoId.classList.remove('select')
     }
 
     let div = document.getElementById(nombreDiv)
